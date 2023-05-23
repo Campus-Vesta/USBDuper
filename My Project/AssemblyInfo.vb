d9a55b43d3feddcccacc5764ca142926683f2259
CampusPeter <p.vandenbroeck@campusvesta.be>
2023-05-23 10:56:27 +0200

    test

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -28,7 +28,7 @@ Imports System.Runtime.InteropServices
 '      Revision
 '
 ' You can specify all the values or you can default the Build and Revision Numbers
-' by using the '*' as shown below:
+' by using the '*' as shown below:test
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
